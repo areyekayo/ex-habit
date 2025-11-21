f76225d63decaae098f01337dcafaf792e4139b4
Riko <riko.fluchel@gmail.com>
2025-11-21 07:45:20 -0500

    add user model

--- server/config.py
+++ server/config.py
@@ -7,11 +7,14 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
 
 # Local imports
 
 # Instantiate app, set attributes
 app = Flask(__name__)
+app.secret_key = b';\x9c\xe1`\\\xb2\x16OOT$\xe4\xd3d\xa2\x9a'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
@@ -23,9 +26,11 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 migrate = Migrate(app, db)
 db.init_app(app)
+ma = Marshmallow(app)
 
 # Instantiate REST API
 api = Api(app)
+bcrypt = Bcrypt(app)
 
 # Instantiate CORS
 CORS(app)
