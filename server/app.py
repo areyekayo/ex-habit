ba19efb251513ea5a4c1d3eeee2e1520d9cd9e4f
Riko <riko.fluchel@gmail.com>
2025-11-23 08:54:51 -0500

    add login resource

--- server/app.py
+++ server/app.py
@@ -3,18 +3,32 @@ from flask import request
 from flask_restful import Resource
 
 from config import app, db, api
-
-from models import User
+from flask_login import login_user, login_required, current_user
 from auth import login_manager
 
-login_manager.init_app(app)
-
+from models import User
 
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
 
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
 
+        user = User.query.filter(User.username == username).first()
+
+        if not user or not user.authenticate(password):
+            return {'errors': {'login': ['Invalid username or password']}}, 401
+        
+        if user.authenticate(password):
+            login_user(user)
+            return user.to_dict, 200
+
+
+api.add_resource(Login, '/login')
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
 
--- server/auth.py
+++ server/auth.py
@@ -1,7 +1,16 @@
 from flask_login import LoginManager
+from flask import jsonify
 from models import User
+from config import app
 
 login_manager = LoginManager()
+login_manager.init_app(app)
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+@login_manager.unauthorized_handler
+def unauthorized_callback():
+    return jsonify({'message': "Unauthorized"})
+
