450cbcfeb52d335f0218f8848c4312c688f3fac6
Riko <riko.fluchel@gmail.com>
2025-12-01 06:50:56 -0500

    seed database

--- server/seed.py
+++ server/seed.py
@@ -7,11 +7,32 @@ from random import randint, choice as rc
 from faker import Faker
 
 # Local imports
-from app import app
-from models import db
+from config import app, db
+from models import User
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
         print("Starting seed...")
-        # Seed code goes here!
+        print("Deleting data...")
+        User.query.delete()
+
+        print("Creating users...")
+
+        riko = User(username="riko")
+        sam = User(username="sam")
+        alex = User(username="alex")
+        steph = User(username="steph")
+        newbie = User(username="newbie")
+        
+        riko.password_hash = riko.username + 'password'
+        sam.password_hash = sam.username + 'password'
+        alex.password_hash = alex.username + 'password'
+        steph.password_hash = steph.username + 'password'
+        newbie.password_hash = newbie.username + 'password'
+        users = [riko, sam, alex, steph, newbie]
+
+        db.session.add_all(users)
+        db.session.commit()
+        print("Seeding done!")
+
