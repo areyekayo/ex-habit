111d5c2f87488cd3c5b9e6b57caf0ad2ed92594a
Riko <riko.fluchel@gmail.com>
2026-01-07 07:27:42 -0500

    fix login redirect

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,19 @@
 import { useState, useContext, useEffect } from "react";
 import LoginForm from "../components/LoginForm";
+import { UserContext } from "../context/UserContext";
+import {useNavigate, useLocation} from "react-router-dom";
 
 function Login(){
     const [showLogin, setShowLogin] = useState(true);
+    const {user} = useContext(UserContext);
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        if (user && location.pathname === "/login") {
+            navigate("/home", {replace: true})
+        }
+    }, [user, navigate, location.pathname])
     
     return (
         <div>
