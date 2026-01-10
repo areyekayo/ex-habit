9495af8a58c009992ac375d076035cf18437f3e0
Riko <riko.fluchel@gmail.com>
2026-01-10 10:05:57 -0500

    add behaviors API resource

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from flask_restful import Resource
 from config import app, db, api, ma
 from flask_login import login_user, login_required, current_user, logout_user
 from auth import login_manager
-from models import User
+from models import User, Behavior
 
 @app.route('/')
 def index():
@@ -49,10 +49,44 @@ class UserSchema(ma.SQLAlchemySchema):
 
 user_schema = UserSchema()
 
+class BehaviorSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Behavior
+        load_instance = True
+
+    id = ma.auto_field()
+    name = ma.auto_field()
+    description = ma.auto_field()
+    type = ma.auto_field()
+
+    url = ma.Hyperlinks(
+        {
+            "self": ma.URLFor(
+                "behaviors",
+                values=dict(id="<id>", name="<name>", description="<description>", type="<type>")
+            )
+        }
+    )
+
+behavior_schema = BehaviorSchema()
+behaviors_schema = BehaviorSchema(many=True)
+
+class Behaviors(Resource):
+    def get(self):
+        behaviors = Behavior.query.all()
+
+        response = make_response(
+            behaviors_schema.dump(behaviors), 200
+        )
+
+        return response
+
+
 
 
 api.add_resource(Login, '/login')
 api.add_resource(Logout, '/logout')
+api.add_resource(Behaviors, '/behaviors')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
