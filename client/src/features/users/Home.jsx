9a396370327c496e666245b4e46a163b1d479215
Riko <riko.fluchel@gmail.com>
2026-01-24 11:31:48 -0500

    add trigger form to home page

--- client/src/features/users/Home.jsx
+++ client/src/features/users/Home.jsx
@@ -1,9 +1,14 @@
 import TriggerCollection from "../triggers/TriggerCollection";
 import { useSelector } from "react-redux";
 import EntryForm from "../journal/EntryForm";
+import { useState } from "react";
+import TriggerForm from "../triggers/TriggerForm";
 
 function Home() {
     const {user} = useSelector((state) => state.user)
+    const [showEntryForm, setShowEntryForm] = useState(false);
+    const [showTriggerForm, setShowTriggerForm] = useState(false);
+
 
     if (!user) return <div>Loading user...</div>
 
@@ -12,7 +17,19 @@ function Home() {
             <h1>Home Page</h1>
             { user ? (<p>Hi, {user.username}</p>)
             : (null)}
-            <EntryForm />
+
+            <button onClick={() => setShowEntryForm(!showEntryForm)}>Add Entry</button>
+
+            {showEntryForm ? (
+                <EntryForm />
+            ) : (<></>)}
+
+            <button onClick={() => setShowTriggerForm(!showTriggerForm)}>Add A Trigger</button>
+
+            {showTriggerForm ? (
+                <TriggerForm />
+            ) : (<></>)}
+        
 
             <TriggerCollection />
         </>
